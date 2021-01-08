90cc9daab6f7711e30bd0e90d04ffb777984b0e6
sk <eduards.gruberts@gmial.com>
2021-01-08 13:39:18 +0200

    add scripts

---
+++ gru.sh
@@ -0,0 +1,15 @@
+
+python3 telemetry-GRU.py GRU 100 100 50
+python3 telemetry-GRU.py GRU 100 200 50
+python3 telemetry-GRU.py GRU 100 300 50
+python3 telemetry-GRU.py GRU 100 400 50
+
+python3 telemetry-GRU.py GRU 100 100 50
+python3 telemetry-GRU.py GRU 200 200 50
+python3 telemetry-GRU.py GRU 300 300 50
+python3 telemetry-GRU.py GRU 400 400 50
+
+python3 telemetry-GRU.py GRU 100 100 50
+python3 telemetry-GRU.py GRU 200 200 25
+python3 telemetry-GRU.py GRU 300 300 10
+python3 telemetry-GRU.py GRU 400 400 5
---
+++ lstm.sh
@@ -0,0 +1,14 @@
+python3 telemetry-lstm.py LSTM 100 100 50
+python3 telemetry-lstm.py LSTM 100 200 50
+python3 telemetry-lstm.py LSTM 100 300 50
+python3 telemetry-lstm.py LSTM 100 400 50
+
+python3 telemetry-lstm.py LSTM 100 100 50
+python3 telemetry-lstm.py LSTM 200 200 50
+python3 telemetry-lstm.py LSTM 300 300 50
+python3 telemetry-lstm.py LSTM 400 400 50
+
+python3 telemetry-lstm.py LSTM 100 100 50
+python3 telemetry-lstm.py LSTM 200 200 25
+python3 telemetry-lstm.py LSTM 300 300 10
+python3 telemetry-lstm.py LSTM 400 400 5
